801d8bd7dfd423d1ce56fe6744e3437708432316
mirror2005 <98074021+mirror2005@users.noreply.github.com>
2022-01-27 09:41:29 +0530

    Update default_commands.py

--- bot/handlers/default_commands.py
+++ bot/handlers/default_commands.py
@@ -14,13 +14,13 @@ async def show_newgame_cb(call: CallbackQuery):
 async def show_newgame_msg(message: Message):
     await message.answer("Press a button below to start a new game (previous one will be dismissed)\n"
                          "Note: 6×6 and 7×7 fields look best on bigger screens or Desktop apps.\n\n"
-                         "Press /help if you're unsure how to play Bombsweeper.",
+                         "Press /help if you're unsure how to play BombBlast.",
                          reply_markup=make_newgame_keyboard())
 
 
 async def cmd_help(message: Message):
     await message.answer("A quick guide how to play Bombsweeper is available here: "
-                         "https://telegra.ph/bombsweeper-how-to-play-09-08")
+                         "https://telegra.ph/BombBlast-Bot-01-27")
 
 
 def register_default_handlers(dp: Dispatcher):
